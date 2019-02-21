1e6114791d3865dd4879d2399f5bda87190d3593
KAWAYu <kawara.yuki@ist.osaka-u.ac.jp>
2019-02-21 10:52:46 +0900

    reduce_ex bug

--- onmt/inputters/dataset_base.py
+++ onmt/inputters/dataset_base.py
@@ -58,7 +58,7 @@ class DatasetBase(Dataset):
 
     def __reduce_ex__(self, proto):
         # This is a hack. Something is broken with torch pickle.
-        return super(DatasetBase, self).__reduce_ex__()
+        return super(DatasetBase, self).__reduce_ex__(proto)
 
     def __init__(self, fields, src1_examples_iter, src2_examples_iter, tgt_examples_iter, filter_pred=None):
 
